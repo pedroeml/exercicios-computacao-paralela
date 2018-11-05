49c422512ded1a3241b24b2070d70d792b566661
pedroeml <pedro.elpidio@acad.pucrs.br>
2018-11-05 18:42:47 -0200

    First working solution of the load balanced version of divide and conquer sorting

--- mpi/divide_conquer.c
+++ mpi/divide_conquer.c
@@ -1,6 +1,6 @@
 /***
  * How to compile: mpicc divide_conquer.c -o divide_conquer -lm
- * 
+ * How to run: mpirun -np <NUMBER-OF-PROCESSES-POWER-OF-2> divide_conquer
  **/
 
 #include <stdio.h>
@@ -9,8 +9,9 @@
 #include <mpi.h>
 
 #define DEBUG 1            // comentar esta linha quando for medir tempo
-#define ARRAY_SIZE 160      // trabalho final com o valores 10.000, 100.000, 1.000.000
+#define ARRAY_SIZE 80      // trabalho final com o valores 10.000, 100.000, 1.000.000
 int delta;
+MPI_Status status;  // Status de retorno
 
 void print_array(int* array, int len) {
     printf("[");
@@ -37,58 +38,63 @@ void bs(int n, int * vetor) {
     }
 }
 
-/** 
- * recebe um ponteiro para um vetor que contem as mensagens recebidas dos filhos e
- * intercala estes valores em um terceiro vetor auxiliar. Devolve um ponteiro para este vetor 
- */
-int *interleaving(int vetor[], int tam) {
-	int *vetor_auxiliar;
-	int i1, i2, i_aux;
-
-	vetor_auxiliar = (int*) malloc(tam*sizeof(int));
-
-	i1 = 0;
-	i2 = tam/2;
-
-	for (i_aux = 0; i_aux < tam; i_aux++) {
-		if (((vetor[i1] <= vetor[i2]) && (i1 < (tam / 2))) || (i2 == tam)) {
-			vetor_auxiliar[i_aux] = vetor[i1++];
-        } else {
-			vetor_auxiliar[i_aux] = vetor[i2++];
-        }
-	}
-
-	return vetor_auxiliar;
+int calc_next_target(int my_rank, int tree_level) {
+    return round(pow(2.0, tree_level) + my_rank);
 }
 
-
-int calc_next_target(int my_rank, int tree_level) {
-    int target = round(pow(2.0, tree_level) + my_rank);
-
-    printf("[%d] Next target: 2^%d+%d = %d\n", my_rank, tree_level, my_rank, target);
-    
-    return target;
+int calc_father(int my_rank, int tree_level) {
+    return round(my_rank - pow(2.0, tree_level));
 }
 
 void sort(int* array, int len, int my_rank) {
     bs(len, array);
 
-    printf("[%d] sorted half_array of len %d\n", my_rank, len);
+    printf("[%d] sorted array of len %d\n", my_rank, len);
     print_array(array, len);
 }
 
-void divide_if_needed(int* array, int len, int target, int my_rank, int tree_level) {
-    printf("[%d] array of len %d\n", my_rank, len);
-    print_array(array, len);
 
+void recursive_join(int* array, int len, int my_rank, int father, int tree_level) {
+    if (tree_level > 0) {
+        if (father > -1 && father != my_rank) {
+            MPI_Send(&len, 1, MPI_INT, father, 1, MPI_COMM_WORLD);
+            MPI_Send(array, len, MPI_INT, father, 1, MPI_COMM_WORLD);
+        } else {
+            int new_tree_level = tree_level - 1;
+            int half_len;
+            int son = calc_next_target(my_rank, new_tree_level);
+            MPI_Recv(&half_len, 1, MPI_INT, son, 1, MPI_COMM_WORLD, &status);
+            
+            int half_array[half_len];
+            MPI_Recv(&half_array, half_len, MPI_INT, son, 1, MPI_COMM_WORLD, &status);
+            
+            int new_array[len+half_len];
+            int i;
+            for (i = 0; i < len; i++) {
+                new_array[i] = *(array + i);
+            }
+            for (i = len; i < len+half_len; i++) {
+                new_array[i] = half_array[i - len];
+            }
+
+            sort(new_array, len+half_len, my_rank);
+
+            if (new_tree_level > 0) {
+                int new_father = calc_father(my_rank, new_tree_level-1);
+                recursive_join(&new_array, len+half_len, my_rank, new_father < 0 ? my_rank : new_father, new_tree_level);
+            }
+        }
+    }
+}
+
+
+void divide_if_needed(int* array, int len, int target, int my_rank, int father, int tree_level) {
     if (len > delta) {
         int half_len = len/2;
         int* half_array = &(array[half_len]);   // The second half of array
         
-        printf("[%d] half_array of len %d\n", my_rank, half_len);
         print_array(half_array, half_len);
-        printf("[%d] Sending data to %d\n", my_rank, target);
-
+        
         int new_tree_level = tree_level + 1;
         MPI_Send(&my_rank, 1, MPI_INT, target, 1, MPI_COMM_WORLD);
         MPI_Send(&new_tree_level, 1, MPI_INT, target, 1, MPI_COMM_WORLD);
@@ -96,24 +102,20 @@ void divide_if_needed(int* array, int len, int target, int my_rank, int tree_lev
         MPI_Send(half_array, half_len, MPI_INT, target, 1, MPI_COMM_WORLD);
 
         if (half_len > delta) {
-            printf("[%d] %d > %d\n", my_rank, half_len, delta);
-            divide_if_needed(array, half_len, calc_next_target(my_rank, new_tree_level), my_rank, new_tree_level);
+            divide_if_needed(array, half_len, calc_next_target(my_rank, new_tree_level), my_rank, my_rank, new_tree_level);
         } else {
             sort(array, half_len, my_rank);
-
-            // TODO: send to father node using interleaving function maybe?
+            recursive_join(array, half_len, my_rank, my_rank, new_tree_level);
         }
     } else {
         sort(array, len, my_rank);
-
-        // TODO: send to father node using interleaving function maybe?
+        recursive_join(array, len, my_rank, father, tree_level);
     }
 }
 
 int main(int argc, char** argv) {
     int my_rank;    // Identificador deste processo
     int proc_n;     // Numero de processos disparados pelo usuário na linha de comando (np)
-    MPI_Status status;  // Status de retorno
 
     MPI_Init(&argc, &argv);     // funcao que inicializa o MPI, todo o código paralelo esta abaixo
 
@@ -130,29 +132,19 @@ int main(int argc, char** argv) {
             vetor[i] = ARRAY_SIZE-i;
         
         int tree_level = 0;
-        divide_if_needed(&vetor, ARRAY_SIZE, calc_next_target(my_rank, tree_level), my_rank, tree_level);
+        divide_if_needed(&vetor, ARRAY_SIZE, calc_next_target(my_rank, tree_level), my_rank, -1, tree_level);
     } else {
-        int i;
         int father;
-        // printf("[%d] Waiting for father node...\n", my_rank);
         MPI_Recv(&father, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
         father = status.MPI_SOURCE;
-
-        printf("[%d] Father is %d\n", my_rank, father);
-
         int tree_level;
         MPI_Recv(&tree_level, 1, MPI_INT, father, 1, MPI_COMM_WORLD, &status);
-        // printf("[%d] Received tree_level %d\n", my_rank, tree_level);
         int len;
         MPI_Recv(&len, 1, MPI_INT, father, 1, MPI_COMM_WORLD, &status);
-        // printf("[%d] Received len %d\n", my_rank, len);
         int half_array[len];
         MPI_Recv(&half_array, len, MPI_INT, father, 1, MPI_COMM_WORLD, &status);
-        printf("[%d] Received half_array\n", my_rank);
         
-        // print_array(&half_array, len);
-        
-        divide_if_needed(&half_array, len, calc_next_target(my_rank, tree_level), my_rank, tree_level);
+        divide_if_needed(&half_array, len, calc_next_target(my_rank, tree_level), my_rank, father, tree_level);
     }
     
     MPI_Finalize();
