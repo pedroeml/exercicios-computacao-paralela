ae9f2dbb406efcd7c5973105a05662a6f3d56720
pedroeml <pedro.elpidio@acad.pucrs.br>
2018-11-13 11:42:22 -0200

    Advances on the fourth assignment implementation

--- mpi/t4/parallel_stages.c
+++ mpi/t4/parallel_stages.c
@@ -6,10 +6,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 #include <mpi.h>
 
 #define ARRAY_SIZE 40
+MPI_Status status;  // Status de retorno
 
 void print_array(int* array, int len) {
     printf("[");
@@ -39,50 +41,70 @@ void bs(int n, int * vetor) {
 void sort(int* array, int len, int my_rank) {
     bs(len, array);
 
-    printf("[%d] Sorted: ");
+    printf("\n[%d] Sorted: ");
     print_array(array, len);
+    printf("\n");
 }
 
-void initialize_array(int* array, int len) {
+void initialize_array(int* array, int len, int my_rank) {
     int i;
     for (i = 0; i < len; i++) {
-        array[i] = len - i;
+        array[i] = (my_rank+1)*len - i;
     }
 }
 
-void parallel_stages(int* array, int len, int my_rank) {
-    int pronto = 0;
+void parallel_stages(int* array, int len, int my_rank, int proc_n) {
+    bool done = false;
+    int states[proc_n];  // Initialize with zeros
+    memset(&states, 0, proc_n);
 
-    while (!pronto) {
+    while (!done) {
         sort(array, len, my_rank); // ordeno vetor local
 
-        // verifico condição de parada
+        if (done)   // verifico condição de parada
+            break;
 
-        // se não for np-1, mando o meu maior elemento para a direita
+        int left_greatest;
+        int my_greatest = *(array + len - 1);
+        
+        if (my_rank != proc_n - 1)  // se não for np-1, mando o meu maior elemento para a direita
+            MPI_Send(&my_greatest, 1, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD);
 
-        // se não for 0, recebo o maior elemento da esquerda
+        if (my_rank != 0)   // se não for 0, recebo o maior elemento da esquerda
+            MPI_Recv(&left_greatest, 1, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD, &status);
+        
+        if (*(array + 0) > left_greatest)   // comparo se o meu menor elemento é maior do que o maior elemento recebido (se sim, estou ordenado em relação ao meu vizinho)
+            states[my_rank] = 1;
 
-        // comparo se o meu menor elemento é maior do que o maior elemento recebido (se sim, estou ordenado em relação ao meu vizinho)
+        int i;
+        for (i = 0; i < proc_n; i++) {
+            MPI_Bcast(&states[i], 1, MPI_INT, i, MPI_COMM_WORLD);    // compartilho o meu estado com todos os processos
+        }
 
-        // compartilho o meu estado com todos os processos
+        int all_sorted = 1;
+        for (i = 0; i < proc_n; i++) {
+            if (states[i] == 0) {
+                all_sorted = 0;
+                break;
+            }
+        }
 
-        // MPI_Bcast(estado);
-
-        // se todos estiverem ordenados com seus vizinhos, a ordenação do vetor global está pronta ( pronto = TRUE, break)
-
-        // senão continuo
-
-        // troco valores para convergir
-
-        // se não for o 0, mando os menores valores do meu vetor para a esquerda
-
-        // se não for np-1, recebo os menores valores da direita
-
-            // ordeno estes valores com a parte mais alta do meu vetor local
-
-            // devolvo os valores que recebi para a direita
-
-        // se não for o 0, recebo de volta os maiores valores da esquerda
+        if (all_sorted) {   // se todos estiverem ordenados com seus vizinhos, a ordenação do vetor global está pronta ( pronto = TRUE, break)
+            done = true;
+            break;
+        } else {    // senão, troco valores para convergir
+            if (my_rank != 0)   // se não for o 0, mando os menores valores do meu vetor para a esquerda
+                MPI_Send(array, len/2, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD);
+            
+            if (my_rank != proc_n - 1) {    // se não for np-1, recebo os menores valores da direita
+                MPI_Recv(array, len/2, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD, &status);                
+                sort(array, len, my_rank); // ordeno estes valores com a parte mais alta do meu vetor local
+                MPI_Send((array + len - 1), len/2, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD);    // devolvo os valores que recebi para a direita
+            }
+                
+            if (my_rank != 0) // se não for o 0, recebo de volta os maiores valores da esquerda
+                MPI_Recv(array, len/2, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD, &status);
+        }
     }
 }
 
@@ -98,19 +120,10 @@ int main(int argc, char** argv) {
     int len = ARRAY_SIZE/proc_n;
     int array[len];
 
-    if (my_rank == 0) {
-        initialize_array(&array, len);
-        printf("[%d]");
-        print_array(&array, len);
-    } else (my_rank < proc_n - 1) {
-        initialize_array(&array, len);
-        printf("[%d]");
-        print_array(&array, len);
-    } else {
-        initialize_array(&array, len);
-        printf("[%d]");
-        print_array(&array, len);
-    }
+    initialize_array(&array, len, my_rank);
+    printf("[%d]", my_rank);
+    print_array(&array, len);
+    parallel_stages(&array, len, my_rank, proc_n);
 
     MPI_Finalize();
     return 0;
