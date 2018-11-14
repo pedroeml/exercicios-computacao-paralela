267eb35aaa5427f3ed4a1912bdd1978fc20de512
pedroeml <pedro.elpidio@acad.pucrs.br>
2018-11-14 19:23:05 -0200

    Includes bug fixes

--- mpi/t4/parallel_stages.c
+++ mpi/t4/parallel_stages.c
@@ -7,10 +7,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <memory.h>
 #include <math.h>
 #include <mpi.h>
 
-#define ARRAY_SIZE 40
+#define ARRAY_SIZE 100000
+#define DELTA ((int)(ARRAY_SIZE*0.025))
 MPI_Status status;  // Status de retorno
 
 void print_array(int* array, int len) {
@@ -40,70 +42,74 @@ void bs(int n, int * vetor) {
 
 void sort(int* array, int len, int my_rank) {
     bs(len, array);
-
-    printf("\n[%d] Sorted: ");
-    print_array(array, len);
-    printf("\n");
 }
 
-void initialize_array(int* array, int len, int my_rank) {
+void initialize_array(int* array, int len, int my_rank, int proc_n) {
     int i;
     for (i = 0; i < len; i++) {
-        array[i] = (my_rank+1)*len - i;
+        array[i] = (proc_n - my_rank)*len - i;
     }
 }
 
-void parallel_stages(int* array, int len, int my_rank, int proc_n) {
-    bool done = false;
-    int states[proc_n];  // Initialize with zeros
-    memset(&states, 0, proc_n);
-
-    while (!done) {
-        sort(array, len, my_rank); // ordeno vetor local
-
-        if (done)   // verifico condição de parada
+bool check_states(int* states, int len) {
+    bool all_sorted = true;
+    int i;
+    for (i = 0; i < len; i++) {
+        if (states[i] == 0) {
+            all_sorted = false;
             break;
+        }
+    }
+    return all_sorted;
+}
+
+void parallel_stages(int len, int my_rank, int proc_n) {
+    int array[len + DELTA];
+    initialize_array(&array, len, my_rank, proc_n);
+    int states[proc_n];  // Initialize with zeros
+    states[0] = 1;
+    int i;
+    for (i = 1; i < proc_n; i++) {
+        states[i] = 0;
+    }
+
+    bool done = false;
+    while (!done) {
+        sort(&array, len, my_rank); // ordeno vetor local
+
+        if (my_rank != proc_n - 1) {    // se não for np-1, mando o meu maior elemento para a direita
+            MPI_Send(&array[len - 1], 1, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD);
+        }
 
         int left_greatest;
-        int my_greatest = *(array + len - 1);
-        
-        if (my_rank != proc_n - 1)  // se não for np-1, mando o meu maior elemento para a direita
-            MPI_Send(&my_greatest, 1, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD);
-
-        if (my_rank != 0)   // se não for 0, recebo o maior elemento da esquerda
+        if (my_rank != 0) {     // se não for 0, recebo o maior elemento da esquerda
             MPI_Recv(&left_greatest, 1, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD, &status);
+            
+            if (array[0] > left_greatest) {    // comparo se o meu menor elemento é maior do que o maior elemento recebido (se sim, estou ordenado em relação ao meu vizinho)
+                states[my_rank] = 1;
+            }
+        }
         
-        if (*(array + 0) > left_greatest)   // comparo se o meu menor elemento é maior do que o maior elemento recebido (se sim, estou ordenado em relação ao meu vizinho)
-            states[my_rank] = 1;
-
-        int i;
         for (i = 0; i < proc_n; i++) {
             MPI_Bcast(&states[i], 1, MPI_INT, i, MPI_COMM_WORLD);    // compartilho o meu estado com todos os processos
         }
 
-        int all_sorted = 1;
-        for (i = 0; i < proc_n; i++) {
-            if (states[i] == 0) {
-                all_sorted = 0;
-                break;
-            }
+        done = check_states(states, proc_n);    // verifico condição de parada
+        if (done)   // se todos estiverem ordenados com seus vizinhos, a ordenação do vetor global está pronta ( pronto = TRUE, break)
+            break;
+
+        if (my_rank != 0) {     // se não for o 0, mando os menores valores do meu vetor para a esquerda
+            MPI_Send(&array[0], DELTA, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD);
         }
 
-        if (all_sorted) {   // se todos estiverem ordenados com seus vizinhos, a ordenação do vetor global está pronta ( pronto = TRUE, break)
-            done = true;
-            break;
-        } else {    // senão, troco valores para convergir
-            if (my_rank != 0)   // se não for o 0, mando os menores valores do meu vetor para a esquerda
-                MPI_Send(array, len/2, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD);
+        if (my_rank != proc_n - 1) {    // se não for np-1, recebo os menores valores da direita
+            MPI_Recv(&array[len], DELTA, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD, &status);
+            sort(&array[len - DELTA], 2*DELTA, my_rank); // ordeno estes valores com a parte mais alta do meu vetor local
+            MPI_Send(&array[len], DELTA, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD);    // devolvo os valores que recebi para a direita
+        }
             
-            if (my_rank != proc_n - 1) {    // se não for np-1, recebo os menores valores da direita
-                MPI_Recv(array, len/2, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD, &status);                
-                sort(array, len, my_rank); // ordeno estes valores com a parte mais alta do meu vetor local
-                MPI_Send((array + len - 1), len/2, MPI_INT, my_rank + 1, 1, MPI_COMM_WORLD);    // devolvo os valores que recebi para a direita
-            }
-                
-            if (my_rank != 0) // se não for o 0, recebo de volta os maiores valores da esquerda
-                MPI_Recv(array, len/2, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD, &status);
+        if (my_rank != 0) {     // se não for o 0, recebo de volta os maiores valores da esquerda
+            MPI_Recv(&array[0], DELTA, MPI_INT, my_rank - 1, 1, MPI_COMM_WORLD, &status);
         }
     }
 }
@@ -118,12 +124,19 @@ int main(int argc, char** argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &proc_n);     // pega informação do numero de processos (quantidade total)
     
     int len = ARRAY_SIZE/proc_n;
-    int array[len];
+    double starttime;
+    
+    if (my_rank == 0) {
+        starttime = MPI_Wtime();
+    }
 
-    initialize_array(&array, len, my_rank);
-    printf("[%d]", my_rank);
-    print_array(&array, len);
-    parallel_stages(&array, len, my_rank, proc_n);
+    parallel_stages(len, my_rank, proc_n);
+
+    if (my_rank == 0) {
+        double stoptime = MPI_Wtime();
+        double executiontime = stoptime - starttime;
+        printf("\nExecution time: %.2f s\n", executiontime);
+    }
 
     MPI_Finalize();
     return 0;
