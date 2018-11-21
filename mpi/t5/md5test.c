c0b25aa19ee568d055bdecaf4a294864408ef2a1
pedroeml <pedro.elpidio@acad.pucrs.br>
2018-11-21 18:51:23 -0200

    First working solution of MPI and Open MP hybrid

--- mpi/t5/md5test.c
+++ mpi/t5/md5test.c
@@ -1,7 +1,15 @@
 /*** 
  * How to compile: 
- * GCC 4.6.3: mpicc md5test.c -o md5test -lcrypto -lssl -std=c99
- * GCC 6.3.0: mpicc md5test.c -o md5test -lcrypto -lssl
+ * GCC 4.6.3: mpicc md5test.c -o md5test -fopenmp -lcrypto -lssl -std=c99
+ * GCC 6.3.0: mpicc md5test.c -o md5test -fopenmp -lcrypto -lssl
+ * 
+ * 5 nodos no máximo
+ * - 1 mestre
+ *      - O mestre só envia a mensagem para os escravos dizendo encontre essa linha e me digam se encontrar ou não.
+ *      - Portanto o escravo irá ficar subutilizando a máquina em que estiver. 
+ * - 5 escravos (6 livros por escravo) 
+ *      - Não pode ser 4 escravos por que são 30 livros.
+ *      - Isso resultaria em 7.5 livros por escravo, o que não é possível fazer. 
  **/
 
 #include <stdio.h>
@@ -11,8 +19,12 @@
 #include <assert.h>
 #include <openssl/md5.h>
 #include <mpi.h>
+#include <omp.h>
 
 #define NUMBER_OF_BOOKS 30
+#define NUMBER_OF_SLAVES 5
+#define NUMBER_OF_BOOKS_BY_SLAVE 6
+#define NUMBER_OF_THREADS 8
 
 MPI_Status status; // estrutura que guarda o estado de retorno
 
@@ -158,16 +170,26 @@ bool md5_equals(unsigned char* md5_a, unsigned char* md5_b) {
     return true;
 }
 
-bool find_line_in_book(Book* book, unsigned char* md5) {
-    int j;
-    for (j = 0; j < book->lines_len; j++) {
-        Line* line = &(book->lines[j]);
-        if (md5_equals(md5, line->md5)) {
-            return true;
+int find_line_in_book(Book* books, unsigned char* md5) {
+    int i;
+    int book_number = -1;
+
+    #pragma omp parallel shared(md5, book_number)
+    #pragma omp for schedule (dynamic)
+    for (i = 0; i < NUMBER_OF_BOOKS_BY_SLAVE; i++) {
+        int j;
+        for (j = 0; j < books[i].lines_len; j++) {
+            Line* line = &(books[i].lines[j]);
+            if (book_number != -1) {
+                break;
+            } else if (md5_equals(md5, line->md5)) {
+                #pragma omp atomic
+                book_number += books[i].number;
+            }
         }
     }
-
-    return false;
+    
+    return book_number;
 }
 
 int find_line_in_books(Book* books, char* line_str) {
@@ -175,17 +197,21 @@ int find_line_in_books(Book* books, char* line_str) {
     int book_number = 0;
     
     int i;
-    for (i = 1; i <= NUMBER_OF_BOOKS; i++) {
+    for (i = 1; i <= NUMBER_OF_SLAVES; i++) {
         MPI_Send(md5, MD5_DIGEST_LENGTH, MPI_UNSIGNED_CHAR, i, 1, MPI_COMM_WORLD);
     }
     
     free(md5);
 
-    for (i = 1; i <= NUMBER_OF_BOOKS; i++) {
-        unsigned short found = 0;
-        MPI_Recv(&found, 1, MPI_UNSIGNED_SHORT, i, 1, MPI_COMM_WORLD, &status);
-        book_number = found != 0 ? i : 0;
-        // printf("\nEscravo[%d]: %s", i, found != 0 ? "encontrou" : "não encontrou");
+    for (i = 1; i <= NUMBER_OF_SLAVES; i++) {
+        int found = 0;
+        MPI_Recv(&found, 1, MPI_INT, i, 1, MPI_COMM_WORLD, &status);
+        book_number = found > 0 ? found : 0;
+        if (book_number > 0) {
+            //printf("\nEscravo[%d]: encontrou no livro %d", i, found);
+        } else {
+            //printf("\nEscravo[%d]: não encontrou", i);
+        }
     }
 
     return book_number;
@@ -216,9 +242,9 @@ size_t number_of_lines_in_books(Book* books) {
     return all_lines_len;
 }
 
-void free_books(Book* books) {
+void free_books(Book* books, int len) {
     int i;
-    for (i = 0; i < NUMBER_OF_BOOKS; i++) {
+    for (i = 0; i < len; i++) {
         int j;
         for (j = 0; j < books[i].lines_len; j++) {
             Line* line = &(books[i].lines[j]);
@@ -260,8 +286,8 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); // pega pega o numero do processo atual (rank)
     MPI_Comm_size(MPI_COMM_WORLD, &proc_n);  // pega informacao do numero de processos (quantidade total)
 
-    if (proc_n != NUMBER_OF_BOOKS + 1) {
-        printf("The number of processes (%d) must be equal to the number of books (%d) + 1 (the master)!\n", proc_n, NUMBER_OF_BOOKS);
+    if (proc_n != NUMBER_OF_SLAVES + 1) {
+        printf("The number of processes (%d) must be equal to the number of slaves (%d) + 1 (the master)!\n", proc_n, NUMBER_OF_SLAVES);
         MPI_Finalize();
         return EXIT_FAILURE;
     }
@@ -269,24 +295,31 @@ int main(int argc, char** argv) {
     if (my_rank == 0) {     // papel do mestre
         Book books[NUMBER_OF_BOOKS];
         
-        int i;
-        for (i = 1; i <= NUMBER_OF_BOOKS; i++) {
-            books[i-1].number = i;
+        int slave;
+        int i = 1;
+        for (slave = 1; slave <= NUMBER_OF_SLAVES; slave++) {
+            int cont;
+            for (cont = 1; cont <= NUMBER_OF_BOOKS_BY_SLAVE; cont++) {
+                books[i - 1].number = i;
 
-            char* whole_text = load_book_i(i);
-            size_t len = strlen(whole_text);
-            
-            MPI_Send(&len, 1, MPI_UNSIGNED_LONG, i, 1, MPI_COMM_WORLD);
-            MPI_Send(whole_text, len, MPI_CHAR, i, 1, MPI_COMM_WORLD);
+                char* whole_text = load_book_i(i);
+                size_t len = strlen(whole_text);
+                
+                MPI_Send(&i, 1, MPI_INT, slave, 1, MPI_COMM_WORLD);
+                MPI_Send(&len, 1, MPI_UNSIGNED_LONG, slave, 1, MPI_COMM_WORLD);
+                MPI_Send(whole_text, len, MPI_CHAR, slave, 1, MPI_COMM_WORLD);
 
-            update_book(&(books[i-1]), whole_text);
+                update_book(&(books[i-1]), whole_text);
 
-            free(whole_text);
+                i++;
+
+                free(whole_text);
+            }
         }
 
         size_t all_lines_len = number_of_lines_in_books(books);
-        for (i = 1; i <= NUMBER_OF_BOOKS; i++) {
-            MPI_Send(&all_lines_len, 1, MPI_UNSIGNED_LONG, i, 1, MPI_COMM_WORLD);
+        for (slave = 1; slave <= NUMBER_OF_SLAVES; slave++) {
+            MPI_Send(&all_lines_len, 1, MPI_UNSIGNED_LONG, slave, 1, MPI_COMM_WORLD);
         }
 
         // books_print(&books);
@@ -297,20 +330,29 @@ int main(int argc, char** argv) {
         double executiontime = stoptime - starttime;
         printf("Execution time: %.2f s\n", executiontime);
 
-        free_books(&books);
+        free_books(&books, NUMBER_OF_BOOKS);
     } else {    // papel do escravo
-        size_t len;
-        MPI_Recv(&len, 1, MPI_UNSIGNED_LONG, 0, 1, MPI_COMM_WORLD, &status);
+        omp_set_num_threads(NUMBER_OF_THREADS);
+        
+        Book books[NUMBER_OF_BOOKS_BY_SLAVE];
+        
+        int i;
+        for (i = 1; i <= NUMBER_OF_BOOKS_BY_SLAVE; i++) {
+            int book_number;
+            MPI_Recv(&book_number, 1, MPI_INT, 0, 1, MPI_COMM_WORLD, &status);
 
-        char* whole_text = (char*) malloc((1+len)*sizeof(char));
-        *(whole_text + len) = '\0';
-        MPI_Recv(whole_text, len, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &status);
+            size_t len;
+            MPI_Recv(&len, 1, MPI_UNSIGNED_LONG, 0, 1, MPI_COMM_WORLD, &status);
 
-        Book book;
-        book.number = my_rank;
-        update_book(&book, whole_text);
+            char* whole_text = (char*) malloc((1+len)*sizeof(char));
+            *(whole_text + len) = '\0';
+            MPI_Recv(whole_text, len, MPI_CHAR, 0, 1, MPI_COMM_WORLD, &status);
 
-        free(whole_text);
+            books[i-1].number = book_number;
+            update_book(&(books[i-1]), whole_text);
+
+            free(whole_text);
+        }
 
         size_t all_lines_len;
         MPI_Recv(&all_lines_len, 1, MPI_UNSIGNED_LONG, 0, 1, MPI_COMM_WORLD, &status);
@@ -320,9 +362,11 @@ int main(int argc, char** argv) {
             unsigned char md5[MD5_DIGEST_LENGTH];
             MPI_Recv(&md5, MD5_DIGEST_LENGTH, MPI_UNSIGNED_CHAR, 0, 1, MPI_COMM_WORLD, &status);
 
-            unsigned short found = find_line_in_book(&book, &md5) ? 1 : 0;
-            MPI_Send(&found, 1, MPI_UNSIGNED_SHORT, 0, 1, MPI_COMM_WORLD);
+            int found = find_line_in_book(&books, &md5);
+            MPI_Send(&found, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
         }
+
+        free_books(&books, NUMBER_OF_BOOKS_BY_SLAVE);
     }
     
     MPI_Finalize();
